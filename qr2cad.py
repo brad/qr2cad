a5be421fff7b6dee06fe97182d74e77e61771b05
Brad Pitcher <bradpitcher@gmail.com>
2011-07-28 22:12:57 -0700

    update to allow any content, misc obvious bug fixes

--- qr2cad.py
+++ qr2cad.py
@@ -9,10 +9,10 @@ import subprocess
 
 def get_args():
 	parser = ArgumentParser(description='Convert qr codes to CAD objects')
+	parser.add_argument('content', metavar='CONTENT', type=str, help='The content you want to create a qr code for.')
 	parser.add_argument('-x', dest='dxf', action='store_const', const=True, default=False, help='Defaults to .stl by default, this option changes output to .dxf')
 	parser.add_argument('-m', dest='make', action='store_const', const=True, default=False, help='Outputs qr2cad.scad by default, this options makes the result into a .dxf or .stl')
-	parser.add_argument('-u', dest='url', type=str, default='http://www.thingiverse.com/thing:10408', help='The url you want to create a qr code for.')
-	parser.add_argument('-d', dest='maxdim', type=int, default=150, help='The maximum size in mm to make the x or y dimension. Only applies if exporting to .scad and/or .stl.')
+	parser.add_argument('-d', dest='maxdim', type=int, default=74, help='The maximum size in mm to make the x or y dimension. Only applies if exporting to .scad and/or .stl.')
 	parser.add_argument('-z', dest='zheight', type=int, default=5, help='The max z-height of the text, defaults to 5. Not relevant for .dxf files')
 	return parser.parse_args()
 
@@ -42,10 +42,12 @@ def create_matrix(data, width):
 	return matrix
 
 def create_scad(matrix, filename, width, height, zheight, maxdim, dxf):
+	scad_width = (width-2)/2
+	scad_height = (height-2)/2
 	if width > height:
-		scale = [float(maxdim)/width, (maxdim*float(height)/width)/height, 1]
+		scale = [float(maxdim)/scad_width, (maxdim*float(scad_height)/scad_width)/scad_height, 1]
 	else:
-		scale = [(maxdim*float(width)/height)/width, float(maxdim)/height, 1]
+		scale = [(maxdim*float(scad_width)/scad_height)/scad_width, float(maxdim)/scad_height, 1]
 	f = open(filename, 'w')
 	f.write("""
 2d = %s;
@@ -54,13 +56,13 @@ block_z = %i;
 block_size = 2;
 matrix_rows = %i;
 matrix_cols = %i;
-""" % ('true' if dxf else 'false', zheight, height, width))
+""" % ('true' if dxf else 'false', zheight, scad_height/2, scad_width/2))
 	# Format the matrix nicely
 	f.write('\nmatrix = [\n')
 	for line in matrix:
 		f.write('%s,\n' % (repr(line)))
 	f.write('];')
-	f.write(display_matrix_core())
+	f.write(display_matrix_core(scale))
 	f.close()
 	print 'SCAD file is '+filename
 
@@ -78,7 +80,7 @@ def make_scad(dxf, scadfilename):
 	subprocess.call(command)
 	print '%s file is %s' % ('DXF' if dxf else 'STL', outfilename)
 
-def display_matrix_core(width, height, dxf):
+def display_matrix_core(scale):
 	return """
 module block(bit, x, y, z, 2d) {
 	if(2d) {
@@ -89,7 +91,7 @@ module block(bit, x, y, z, 2d) {
 	}	
 }
 
-translate([-block_size*matrix_cols/2, block_size*matrix_rows/2, 0]) {
+scale(%s) translate([-block_size*matrix_cols/2, block_size*(matrix_rows/2-1), 0]) {
 	if( ! 2d) {
 		translate([0, -block_size*(matrix_rows-1), 0]) {
 			cube([block_size*matrix_cols, block_size*matrix_rows, 1]);
@@ -122,20 +124,20 @@ translate([-block_size*matrix_cols/2, block_size*matrix_rows/2, 0]) {
 		}
 	}
 }
-"""
+""" % (repr(scale))
 
 if __name__ == '__main__':
 	args = get_args()
 	filename = 'qr2cad.scad'
 
-	# Generates an RGB array, given a url, using the google chart API
-	[data, width, height] = get_image_data(args.url)
+	# Generates an RGB array, given content, using the google chart API
+	[data, width, height] = get_image_data(args.content)
 
 	# Outputs a matrix that OpenSCAD can use
 	matrix = create_matrix(data, width)
 
 	# Outputs a .scad file that can be used to create a .stl or .dxf file
-	create_scad(matrix, filename, width, height, args.maxdim, args.dxf)
+	create_scad(matrix, filename, width, height, args.zheight, args.maxdim, args.dxf)
 	if args.make:
 		# Outputs a .dxf or .stl
 		make_scad(args.dxf, filename)
